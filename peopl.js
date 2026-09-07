b4a0dc76ec3266138fe69dd97fb84072c02e9a63
abczendeboudi-design <abczendeboudi@gmail.com>
2026-09-07 22:57:25 +0330

    Update peopl.js

--- peopl.js
+++ peopl.js
@@ -1,289 +1,32 @@
 const people = {
 
-    // =========================
-    // نسل اول
-    // =========================
-
-    "gholamhossein-older": {
-        name: "غلامحسین",
-        gender: "مرد",
-
-        father: "",
-        mother: "",
-        spouse: ["احمد"],
-
-        children: ["احمد"],
-
-        job: "",
-        education: "",
-
-        bio: ""
-    },
-
-
-    // =========================
-    // احمد و شیرین
-    // =========================
-
-    "ahmad": {
-        name: "احمد",
-        gender: "مرد",
-
-        father: "غلامحسین",
-        mother: "",
-        spouse: ["شیرین"],
-
-        children: [
-            "فریده",
-            "فجر",
-            "غلامحسین",
-            "فیض",
-            "پدرام"
-        ],
-
-        job: "",
-        education: "",
-
-        bio: "احمد فرزند غلامحسین است."
-    },
-
-    "shirin": {
-        name: "شیرین",
-        gender: "زن",
-
-        father: "",
-        mother: "",
-        spouse: ["احمد"],
-
-        children: [
-            "فریده",
-            "فجر",
-            "غلامحسین",
-            "فیض",
-            "پدرام"
-        ],
-
-        job: "",
-        education: "",
-
-        bio: ""
-    },
-
-
-    // =========================
-    // فریده و صادق
-    // =========================
-
-    "farideh": {
-        name: "فریده",
-        gender: "زن",
-
-        father: "احمد",
-        mother: "شیرین",
-        spouse: ["صادق"],
-
-        children: ["کاملیا"],
-
-        job: "کارمند",
-        education: "دکترای شیلات",
-
-        bio: ""
-    },
-
-    "sadegh": {
-        name: "صادق",
-        gender: "مرد",
-
-        father: "",
-        mother: "",
-        spouse: ["فریده"],
-
-        children: ["کاملیا"],
-
-        job: "",
-        education: "",
-
-        bio: ""
-    },
-
-    "kamelya": {
-        name: "کاملیا",
-        gender: "دختر",
-
-        father: "صادق",
-        mother: "فریده",
-        spouse: [],
-
-        children: [],
-
-        job: "",
-        education: "",
-
-        bio: ""
-    },
-
-
-    // =========================
-    // فجر و فاطمه
-    // =========================
-
-    "fajr": {
-        name: "فجر",
-        gender: "مرد",
-
-        father: "احمد",
-        mother: "شیرین",
-        spouse: ["فاطمه"],
-
-        children: ["باران"],
-
-        job: "کارمند",
-        education: "فوق‌لیسانس برق",
-
-        bio: ""
-    },
-
-    "fatemeh": {
-        name: "فاطمه",
-        gender: "زن",
-
-        father: "",
-        mother: "",
-        spouse: ["فجر"],
-
-        children: ["باران"],
-
-        job: "",
-        education: "",
-
-        bio: ""
-    },
-
-    "baran": {
-        name: "باران",
-        gender: "دختر",
-
-        father: "فجر",
-        mother: "فاطمه",
-        spouse: [],
-
-        children: [],
-
-        job: "",
-        education: "",
-
-        bio: ""
-    },
-
-
-    // =========================
-    // غلامحسین و سمیه
-    // =========================
-
-    "gholamhossein-ahmad": {
-        name: "غلامحسین",
-        gender: "مرد",
-
-        father: "احمد",
-        mother: "شیرین",
-        spouse: ["سمیه"],
-
-        children: ["لیانا"],
-
-        job: "",
-        education: "",
-
-        bio: ""
-    },
-
-    "somayeh-gholamhossein": {
-        name: "سمیه",
-        gender: "زن",
-
-        father: "",
-        mother: "",
-        spouse: ["غلامحسین"],
-
-        children: ["لیانا"],
-
-        job: "کارمند",
-        education: "فوق‌لیسانس شیلات",
-
-        bio: ""
-    },
-
-    "liyana": {
-        name: "لیانا",
-        gender: "دختر",
-
-        father: "غلامحسین",
-        mother: "سمیه",
-        spouse: [],
-
-        children: [],
-
-        job: "",
-        education: "",
-
-        bio: ""
-    },
-
-
-    // =========================
-    // فیض و سمیه
-    // =========================
-
-    "faiz": {
-        name: "فیض",
-        gender: "مرد",
-
-        father: "احمد",
-        mother: "شیرین",
-        spouse: ["سمیه"],
-
-        children: [],
-
-        job: "کارمند",
-        education: "فوق‌لیسانس ادبیات",
-
-        bio: ""
-    },
-
-    "somayeh-faiz": {
-        name: "سمیه",
-        gender: "زن",
-
-        father: "",
-        mother: "",
-        spouse: ["فیض"],
-
-        children: [],
-
-        job: "",
-        education: "",
-
-        bio: ""
-    },
-
-
-    // =========================
-    // پدرام
-    // =========================
-
-    "pedram": {
-        name: "پدرام",
-        gender: "مرد",
-
-        father: "احمد",
-        mother: "شیرین",
-        spouse: [],
-
-        children: [],
-
-        job: "",
-        education: "",
-
-        bio: ""
-    }
+  ahmad: {
+    name: "احمد",
+    father: "gholamhossein",
+    spouse: ["shirin"],
+    children: [
+      "farideh",
+      "fajr",
+      "gholamhossein-ahmad",
+      "feiz",
+      "pedram"
+    ],
+    job: "معلم",
+    education: ""
+  },
+
+  shirin: {
+    name: "شیرین",
+    spouse: ["ahmad"],
+    children: [
+      "farideh",
+      "fajr",
+      "gholamhossein-ahmad",
+      "feiz",
+      "pedram"
+    ],
+    job: "خانه دار",
+    education: ""
+  }
 
 };
