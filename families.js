a22bd46215bccdd84bd08c9ecdc87f3faf89c046
abczendeboudi-design <abczendeboudi@gmail.com>
2026-09-11 19:02:30 +0330

    Create families.js

---
+++ families.js
@@ -0,0 +1,11 @@
+window.GENEALOGY_DATA = {
+    families: [
+        {
+            id: "gholamhossein-qasem",
+            number: 1,
+            name: "خاندان غلامحسین قاسم زائر غلامحسین",
+            root: "gholamhossein-qasem-zayer-gholamhossein",
+            image: "images/genealogy/gholamhossein-qasem.png"
+        }
+    ]
+};
