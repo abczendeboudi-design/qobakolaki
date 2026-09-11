802f61f790989d72b1f97ae2cdc49e08b1ea7200
abczendeboudi-design <abczendeboudi@gmail.com>
2026-09-11 19:58:16 +0330

    Update families.js

--- families.js
+++ families.js
@@ -1,11 +1,12 @@
 window.GENEALOGY_DATA = {
-    families: [
-        {
-            id: "gholamhossein-qasem",
-            number: 1,
-            name: "خاندان غلامحسین قاسم زائر غلامحسین",
-            root: "gholamhossein-qasem-zayer-gholamhossein",
-            image: "images/genealogy/gholamhossein-qasem.png"
-        }
-    ]
+  families: [
+    {
+      id: "gholamhossein-qasem",
+      number: 1,
+      name: "خاندان غلامحسین قاسم زائر غلامحسین",
+      image: "images/genealogy/gholamhossein-qasem.png",
+      root: "gholamhossein-qasem-zayer-gholamhossein",
+      memberCount: 0
+    }
+  ]
 };
