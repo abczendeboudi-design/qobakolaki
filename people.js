30247962d4167f6ef67a630cc7d1f64835b46d44
abczendeboudi-design <abczendeboudi@gmail.com>
2026-09-11 19:07:27 +0330

    Create people.js

---
+++ people.js
@@ -0,0 +1,61 @@
+window.GENEALOGY_DATA = window.GENEALOGY_DATA || {};
+
+window.GENEALOGY_DATA.people = {
+
+    "gholamhossein-qasem-zayer-gholamhossein": {
+        id: "gholamhossein-qasem-zayer-gholamhossein",
+        name: "غلامحسین قاسم زائر غلامحسین",
+        gender: "male",
+        occupation: "",
+        education: "",
+        birthDate: "",
+        deathDate: "",
+        birthPlace: "",
+        residence: "",
+        photo: "",
+        notes: ""
+    },
+
+    "parizad": {
+        id: "parizad",
+        name: "پریزاد",
+        gender: "female",
+        occupation: "",
+        education: "",
+        birthDate: "",
+        deathDate: "",
+        birthPlace: "",
+        residence: "",
+        photo: "",
+        notes: ""
+    },
+
+    "qasem": {
+        id: "qasem",
+        name: "قاسم",
+        gender: "male",
+        occupation: "",
+        education: "",
+        birthDate: "",
+        deathDate: "",
+        birthPlace: "",
+        residence: "",
+        photo: "",
+        notes: ""
+    },
+
+    "iran-vali": {
+        id: "iran-vali",
+        name: "ایران ولی",
+        gender: "female",
+        occupation: "",
+        education: "",
+        birthDate: "",
+        deathDate: "",
+        birthPlace: "",
+        residence: "",
+        photo: "",
+        notes: ""
+    }
+
+};
